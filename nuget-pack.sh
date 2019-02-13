34e28ebd4a0fa7b75d66318b53d5edfa8150b527
CompulsiveCoder <compulsivecoder@gmail.com>
2019-02-13 03:10:12 +0000

    Fixing nuget pack script

--- nuget-pack.sh
+++ nuget-pack.sh
@@ -7,11 +7,12 @@ mv -f pkg/*.nupkg pkg/archive
 
 BRANCH=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
 
-if [ "$BRANCH" = "dev" ]
-then    
+# TODO: Remove if not needed. Disabled because version increment is triggered in jenkinsfile
+#if [ "$BRANCH" = "dev" ]
+#then    
     # Only increment version during dev builds. The dev version can be used when graduated to the master branch
-    sh increment-version.sh
-fi
+#    sh increment-version.sh
+#fi
 
 VERSION=$(cat version.txt)
 BUILD_NUMBER=$(cat buildnumber.txt)
