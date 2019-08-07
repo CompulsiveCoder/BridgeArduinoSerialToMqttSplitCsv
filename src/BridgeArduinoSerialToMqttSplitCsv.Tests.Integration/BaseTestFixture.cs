3abdee2cd3381428c2aafd527cf55e5fb199b555
CompulsiveCoder <compulsivecoder@gmail.com>
2019-08-08 07:23:39 +1000

    Added app.config.default

--- src/BridgeArduinoSerialToMqttSplitCsv.Tests.Integration/BaseTestFixture.cs
+++ src/BridgeArduinoSerialToMqttSplitCsv.Tests.Integration/BaseTestFixture.cs
@@ -6,75 +6,72 @@ using System.IO;
 
 namespace BridgeArduinoSerialToMqttSplitCsv.Tests.Integration
 {
-	public class BaseTestFixture
-	{
-		public BaseTestFixture()
-		{
-		}
+    public class BaseTestFixture
+    {
+        public BaseTestFixture ()
+        {
+        }
 
-		[SetUp]
-		public void Initialize()
-		{
-			Console.WriteLine("");
-			Console.WriteLine("====================");
-			Console.WriteLine("Preparing test");
-		}
+        [SetUp]
+        public void Initialize ()
+        {
+            Console.WriteLine ("");
+            Console.WriteLine ("====================");
+            Console.WriteLine ("Preparing test");
+        }
 
-		[TearDown]
-		public virtual void Finish()
-		{
-			HandleFailureFile();
+        [TearDown]
+        public virtual void Finish ()
+        {
+            HandleFailureFile ();
 
-			Console.WriteLine("Finished test");
-			Console.WriteLine("====================");
-			Console.WriteLine("");
-		}
+            Console.WriteLine ("Finished test");
+            Console.WriteLine ("====================");
+            Console.WriteLine ("");
+        }
 
-		public void HandleFailureFile()
-		{
-			var failuresDir = Path.GetFullPath("../../failures");
+        public void HandleFailureFile ()
+        {
+            var failuresDir = Path.GetFullPath ("../../failures");
 
-			var fixtureName = TestContext.CurrentContext.Test.FullName;
+            var fixtureName = TestContext.CurrentContext.Test.FullName;
 
-			var failureFile = Path.Combine(failuresDir, fixtureName + ".txt");
+            var failureFile = Path.Combine (failuresDir, fixtureName + ".txt");
 
-			if (TestContext.CurrentContext.Result.State == TestState.Error
-			  || TestContext.CurrentContext.Result.State == TestState.Failure)
-			{
-				Console.WriteLine("Test failed.");
+            if (TestContext.CurrentContext.Result.State == TestState.Error
+            || TestContext.CurrentContext.Result.State == TestState.Failure) {
+                Console.WriteLine ("Test failed.");
 
-				Console.WriteLine(failuresDir);
-				Console.WriteLine(fixtureName);
-				Console.WriteLine(failureFile);
+                Console.WriteLine (failuresDir);
+                Console.WriteLine (fixtureName);
+                Console.WriteLine (failureFile);
 
-				if (!Directory.Exists(failuresDir))
-					Directory.CreateDirectory(failuresDir);
+                if (!Directory.Exists (failuresDir))
+                    Directory.CreateDirectory (failuresDir);
 
-				File.WriteAllText(failureFile, fixtureName);
-			}
-			else
-			{
-				Console.WriteLine("Test passed.");
-				if (File.Exists(failureFile))
-					File.Delete(failureFile);
-			}
-		}
+                File.WriteAllText (failureFile, fixtureName);
+            } else {
+                Console.WriteLine ("Test passed.");
+                if (File.Exists (failureFile))
+                    File.Delete (failureFile);
+            }
+        }
 
-		public string GetDevicePort()
-		{
-			var devicePort = Environment.GetEnvironmentVariable("MQTT_BRIDGE_EXAMPLE_DEVICE_PORT");
+        public string GetDevicePort ()
+        {
+            var devicePort = Environment.GetEnvironmentVariable ("MQTT_BRIDGE_EXAMPLE_DEVICE_PORT");
 
-			if (String.IsNullOrEmpty(devicePort))
-				devicePort = "/dev/ttyUSB1";
+            if (String.IsNullOrEmpty (devicePort))
+                devicePort = "/dev/ttyUSB0";
 
-			Console.WriteLine("Device port: " + devicePort);
+            Console.WriteLine ("Device port: " + devicePort);
 
-			return devicePort;
-		}
+            return devicePort;
+        }
 
-		public int GetDeviceSerialBaudRate()
-		{
-			return 9600;
-		}
-	}
+        public int GetDeviceSerialBaudRate ()
+        {
+            return 9600;
+        }
+    }
 }
